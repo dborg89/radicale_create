f90ba48002874fc103366d176f5a3f692840f591
David B <dborg@flu.id.au>
2022-02-27 13:18:55 +1100

    Update calsync.py
    Added: 
    - Permission changes to files and folders to ensure they are all under the radicale folders UID and GID.

--- calsync.py
+++ calsync.py
@@ -6,6 +6,7 @@ import psycopg2
 import string
 import random
 import pathlib
+import os
 
 # Set the working DIR to Modoboa
 sys.path.insert(0, "/srv/modoboa/instance/")
@@ -32,7 +33,7 @@ cur2 = conn.cursor()
 search_query = """SELECT admin_mailbox.user_id,admin_mailbox.address,admin_mailbox.domain_id,admin_domain.name,admin_mailbox.id
       FROM admin_mailbox 
       JOIN admin_domain ON admin_domain.id = admin_mailbox.domain_id
-      WHERE NOT EXISTS ( SELECT mailbox_id FROM radicale_usercalendar WHERE radicale_usercalendar.mailbox_id = admin_mailbox.id )"""
+      WHERE NOT EXISTS ( SELECT mailbox_id FROM radicale_usercalendar WHERE radicale_usercalendar.mailbox_id = admin_mailbox.id AND radicale_usercalendar.name = '%s')""" % ("Calendar")
 
 # Run the query to check and start looping through the results
 cur.execute(search_query)
@@ -50,11 +51,21 @@ for chuser in cur:
     conn.commit()
    
     # Check if the DIR exists for the calendar file and if not make items
-    cal_path = pathlib.Path(radicale_path + "/" + chuser[1] + "@" + chuser[3] + "/Calendar/")
-    pathlib.Path(cal_path)
+    user_cal_path = radicale_path + chuser[1] + "@" + chuser[3] + "/Calendar/"
+    cal_path = pathlib.Path(user_cal_path)
     cal_path.mkdir(parents=True, exist_ok=True)
 
     # Make the prop file for Radicale
     file_path = cal_path / ".Radicale.props"
     with file_path.open("w", encoding ="utf-8") as f:
         f.write("{\"tag\": \"VCALENDAR\"}")
+        
+# Set Permissions to calendar folder and prop file
+radu = os.stat(radicale_path).st_uid
+radg = os.stat(radicale_path).st_gid
+
+for root, subdirectories, files in os.walk(radicale_path):
+    for subdirectory in subdirectories:
+        os.chown(os.path.join(root, subdirectory),radu,radg)
+    for file in files:
+        os.chown(os.path.join(root, file),radu,radg)        
